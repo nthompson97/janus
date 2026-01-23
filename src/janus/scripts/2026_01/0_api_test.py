879e54cb2e497b5fd1897d57b4d05ebdaf9d17f2
Nicholas Thompson <njthompson1004@gmail.com>
2026-01-23 16:39:24 +1000

    more testing websocket

--- src/janus/scripts/2026_01/0_api_test.py
+++ src/janus/scripts/2026_01/0_api_test.py
@@ -5,8 +5,32 @@ import nest_asyncio
 
 async def main() -> None:
     async with HyperLiquidAPI() as api:
-        meta = await api.spot_meta()
-        print(meta)
+        spot_meta = await api.spot_meta()
+
+        coin_to_asset = {}
+        name_to_coin = {}
+        asset_to_sz_decimals = {}
+
+        for i, spot in enumerate(spot_meta["universe"]):
+            name = spot["name"]
+            asset = spot["index"] + 10000
+
+            base, quote = spot["tokens"]
+
+            base_info = spot_meta["tokens"][base]
+            quote_info = spot_meta["tokens"][quote]
+
+            coin_to_asset[name] = asset
+            name_to_coin[name] = name
+            asset_to_sz_decimals[asset] = base_info["szDecimals"]
+
+        # add the perpetuals in
+        meta = await api.meta()
+
+        for asset, asset_info in enumerate(meta["universe"]):
+            coin_to_asset[asset_info["name"]] = asset
+            name_to_coin[asset_info["name"]] = asset_info["name"]
+            asset_to_sz_decimals[asset] = asset_info["szDecimals"]
 
         from IPython import embed
 
---
+++ src/janus/scripts/2026_01/1_websocket_test.py
@@ -0,0 +1,65 @@
+import asyncio
+import json
+import logging
+
+import websockets
+
+from janus.core.metadata import Coin, BTC, ETH
+
+TIMEOUT = 3
+MAX_COUNT = 10
+
+WS_URL = "wss://api.hyperliquid-testnet.xyz/ws"  # testnet
+
+
+async def subscribe_bbo(ws: websockets.ClientConnection, coin: type[Coin]) -> None:
+    msg = {
+        "method": "subscribe",
+        "subscription": {"type": "bbo", "coin": coin.name},
+    }
+
+    await ws.send(json.dumps(msg))
+    logging.info(f"Subscribed to BBO for {coin}")
+
+
+async def main() -> None:
+    while True:
+        try:
+            async with websockets.connect(WS_URL) as ws:
+                logging.info(f"Connected to {WS_URL}")
+
+                coin = "BTC"
+                msg = {
+                    "method": "subscribe",
+                    "subscription": {"type": "bbo", "coin": coin},
+                }
+
+                await ws.send(json.dumps(msg))
+                logging.info(f"Subscribed to BBO for {coin}")
+
+                count = 0
+
+                async for message in ws:
+                    data = json.loads(message)
+
+                    logging.info(f"Iteration {count}")
+                    logging.info(message)
+                    logging.info(data)
+
+                    count += 1
+
+                    if count > MAX_COUNT:
+                        break
+
+                break
+
+        except Exception as e:
+            logging.error(f"Disconnect from {WS_URL}")
+            logging.error(e)
+
+            logging.info(f"Retrying in {TIMEOUT} seconds")
+            await asyncio.sleep(TIMEOUT)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
